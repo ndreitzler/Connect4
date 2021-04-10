aa11d5c49d25dc38bc041a83797d809fab665e29
Nick Dreitzler <ndreitz@clemson.edu>
2021-04-10 18:43:02 -0400

    done integrating

--- Connect4.ino
+++ Connect4.ino
@@ -12,6 +12,7 @@
 #include "CrankShaft.hpp"
 #include "Dropper.hpp"
 #include "SorterMotor.hpp"
+#include "Corkscrew.hpp"
 
 #define ROWS 4 //Rows of keypad
 #define COLS 4 //Columns of keypad
@@ -65,13 +66,15 @@ void processUserInput(char keyPress)
   static CrankShaft crankShaft(CS_STEP_PIN, CS_DIR_PIN, CS_EN_PIN, CS_MICRO_STEP, CS_US_DELAY);
   static Dropper dropper(DROP_STEP_PIN, DROP_DIR_PIN, DROP_EN_PIN, DROP_MICRO_STEP, DROP_US_DELAY, A0);
   static SorterMotor sorterMotor(SORTER_STEP_PIN, SORTER_DIR_PIN, SORTER_EN_PIN, SORTER_MICRO_STEP, SORTER_US_DELAY, SORTER_BUTTON);
+  static Corkscrew purpleCS(CORK_PURPLE_STEP_PIN, CORK_PURPLE_DIR_PIN, CORK_PURPLE_EN_PIN, CORK_PURPLE_MICRO_STEP, CORK_PURPLE_US_DELAY);
+  static Corkscrew orangeCS(CORK_ORANGE_STEP_PIN, CORK_ORANGE_DIR_PIN, CORK_ORANGE_EN_PIN, CORK_ORANGE_MICRO_STEP, CORK_ORANGE_US_DELAY);
   static bool isGameOver = false;
   
   //Process Input
 
   if (keyPress == 'D') //Reset the game, release tokens and sort them, prepare for new game
   {
-    resetAll(MasterGame, sorter, crankShaft, dropper, sorterMotor);
+    resetAll(MasterGame, sorter, crankShaft, dropper, sorterMotor, purpleCS, orangeCS);
     isGameOver = false;
   }
   else if(isGameOver) //The Game is over don't process new game move
@@ -81,17 +84,12 @@ void processUserInput(char keyPress)
   }
   else if (keyPress >= '1' && keyPress <= '7') //Vaild move, drop token in chosen column
   {
-    Serial.println(keyPress);
-    //dropper.printCols();
-    //dropper.moveDispenser(keyPress - '1');
-    //crankShaft.advanceRelease();
-    //crankShaft.moveMotor(200*16, false);
-    //sorterMotor.initSorter();
-    isGameOver = playGame(MasterGame, dropper, keyPress - '1');
+    //Serial.println(keyPress);
+    isGameOver = playGame(MasterGame, dropper, keyPress - '1', purpleCS, orangeCS);
   }
 }
 
-bool playGame(Game &MasterGame, Dropper &dropper, byte keyPress)
+bool playGame(Game &MasterGame, Dropper &dropper, byte keyPress, Corkscrew &purpleCS, Corkscrew &orangeCS)
 {
   Solver solver;
   int AImove;
@@ -100,7 +98,7 @@ bool playGame(Game &MasterGame, Dropper &dropper, byte keyPress)
   if(MasterGame.canPlay(keyPress)) // a vaild move was made
   {
     Serial.println("playing");
-    dropToken(dropper, keyPress); //Drop human token
+    dropToken(dropper, keyPress, orangeCS); //Drop human token
     MasterGame.makeHumanMove(keyPress); //Record human move
     //MasterGame.printGame();
     if(MasterGame.checkWin(true)) //The human has won the game
@@ -110,7 +108,7 @@ bool playGame(Game &MasterGame, Dropper &dropper, byte keyPress)
     } else { //The game is not over and the AI must make a move
       AImove = solver.decideAIMove(MasterGame); //Get AI move
       delay(3000);
-      dropToken(dropper, AImove); //Drop AI's token
+      dropToken(dropper, AImove, purpleCS); //Drop AI's token
       MasterGame.makeAIMove(AImove); //Record AI move
       if(MasterGame.checkWin(false)) //Check if the AI has won
       {
@@ -128,16 +126,19 @@ bool playGame(Game &MasterGame, Dropper &dropper, byte keyPress)
   return isGameOver;
 }
 
-void dropToken(Dropper &dropper, byte keyPress)
+void dropToken(Dropper &dropper, byte keyPress, Corkscrew &color)
 {
   dropper.moveDispenser(keyPress);
+  color.moveUpOne();
 }
 
 
 //Reset the game. Release and sort pieces, clear game record, and move dropper to center column
-void resetAll(Game &MasterGame, Sorter &sorter, CrankShaft &crankShaft, Dropper &dropper, SorterMotor &sorterMotor)
+void resetAll(Game &MasterGame, Sorter &sorter, CrankShaft &crankShaft, Dropper &dropper, SorterMotor &sorterMotor, Corkscrew &purpleCS, Corkscrew &orangeCS)
 {
-    sorter.sortPieces(MasterGame, crankShaft, sorterMotor);
+    purpleCS.dropToBottom();
+    orangeCS.dropToBottom();
+    sorter.sortPieces(MasterGame, crankShaft, sorterMotor, purpleCS, orangeCS);
     MasterGame.fullReset();
     
 }
--- Sorter.hpp
+++ Sorter.hpp
@@ -10,11 +10,12 @@
 #include "Game.hpp"
 #include "CrankShaft.hpp"
 #include "SorterMotor.hpp"
+#include "Corkscrew.hpp"
 
 class Sorter 
 {
  public:
-  void sortPieces(Game &board, CrankShaft &crankShaft, SorterMotor &sorterMotor)
+  void sortPieces(Game &board, CrankShaft &crankShaft, SorterMotor &sorterMotor, Corkscrew &purpleCS, Corkscrew &orangeCS)
   {
 
     // Var initializations
@@ -28,11 +29,16 @@ class Sorter
     for (i = 6; i >= 0; --i) {
       byte grid = board.getGridCol(i);
       byte mask = board.getMaskCol(i);
-      //printf("Drop Column #%d\n", i+1);         // Replace with what makes column actually drop
       Current_MASK_Value  = mask & BIT_SEL;  // Is there a piece
       Current_POS_Value = grid & BIT_SEL;  // Direction to move sorter
       while (Current_MASK_Value) {
         sorterMotor.moveSorterFlap((bool)Current_POS_Value);
+        if(Current_POS_Value)
+        {
+          orangeCS.moveUpOne();
+        } else {
+          purpleCS.moveUpOne();
+        }
         BIT_SEL = BIT_SEL << 1;           //Shift the bit being selected left
         Current_MASK_Value  =  mask & BIT_SEL; // Is there a piece
         Current_POS_Value = grid & BIT_SEL;// Direction to move sorter
@@ -42,7 +48,6 @@ class Sorter
       BIT_SEL = 0x01;
     }
     crankShaft.advanceRelease(); // Close last column
-    //printf("Done Sorting\n");               // Replace with what happens after sorting
   }
 
 };
